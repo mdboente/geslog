70d7a8d636bb9c6f847fd36eacc4e8a4c1f9f9f0
Iram Morales <iram.morales@generalsoftwareinc.com>
2022-05-28 17:58:16 -0400

    Set filter to client and item_groups fields of Doctype Cost Center

---
+++ geslog/custom_scripts/cost_center/cost_center.js
@@ -0,0 +1,14 @@
+
+
+frappe.ui.form.on("Cost Center", {
+	setup(frm){
+
+		frm.set_query("client", () => {
+			return {filters: { is_client: 1}}
+		})
+
+		frm.set_query("item_group", "item_group", () => {
+			return { filters: { associate_to_cost_center: 1}}
+		})
+	},
+})
