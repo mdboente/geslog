d131b6fa21724e54bbac047b3889e8f4163f7640
Iram Morales <iram.morales@generalsoftwareinc.com>
2022-07-24 18:50:15 -0400

    Add Client doctype

---
+++ geslog/geslog/doctype/client/client.js
@@ -0,0 +1,31 @@
+
+frappe.ui.form.on('Client', {
+	access_to_all_warehouses_with_all_operations(frm){
+	    frm.doc.warehouse = []
+        if(frm.doc.access_to_all_warehouses_with_all_operations){
+            frappe.db.get_list(
+	        "Warehouse",
+            {
+                filters: {"is_group": false},
+                fields: ["name", "type"]
+            }).then(resp => {
+                    resp.forEach(warehouse => {
+                        const default_operation = "ALL";
+                        frm.add_child("warehouse", {
+                            "warehouse": warehouse.name,
+                            "warehouse_type": warehouse.type,
+                            "operation": default_operation
+                        })
+                        frm.refresh_field("warehouse")
+                    })
+                })
+        }
+        frm.refresh_field("warehouse")
+    }
+});
+
+frappe.ui.form.on("Client Warehouse", {
+    warehouse_add(frm){
+        geslog.form.select_not_checked_links(frm, "warehouse", "warehouse")
+    }
+})
